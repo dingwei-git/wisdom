aac61e90993b8b2b105fcf2c113ca4a103bd446d
834851969@qq.com <19900718dingwei>
2021-01-13 11:36:27 +0800

    vue

---
+++ my-vue/src/components/HelloWorld.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="hello">
+    <div class="topzi">智慧物联平台登陆</div>
+    <div class="topzi">
+      <el-form-item prop="name" label="姓名">
+          <input type="text" v-model="city"/>
+      </el-form-item>
+      <!-- <el-form :model="ruleForm2" status-icon :rules="rules2" ref="ruleForm2" label-width="100px" class="demo-ruleForm">
+        <el-form-item prop="name" label="姓名">
+          <el-input v-model="ruleForm2.name"></el-input>
+        </el-form-item>
+        <el-form-item label="密码" prop="pass">
+          <el-input type="password" v-model="ruleForm2.pass" auto-complete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="确认密码" prop="checkPass">
+          <el-input type="password" v-model="ruleForm2.checkPass" auto-complete="off"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="submitForm('ruleForm2')">提交</el-button>
+          <el-button @click="resetForm('ruleForm2')">重置</el-button>
+        </el-form-item>
+      </el-form> -->
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      city:''
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ my-vue/src/components/login.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="hello">
+    <div>智慧物联平台登陆</div>
+    <div>
+      <div>
+        <el-form-item prop="name" label="账号">
+          <input type="text" v-model="loginName"/>
+        </el-form-item>
+      </div>
+      <div>
+        <el-form-item label="密码" prop="pass">
+          <input type="password" v-model="password"/>
+        </el-form-item>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+    export default {
+    data () {
+        return {
+            loginName:'',
+            password:''
+        }
+    }
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
